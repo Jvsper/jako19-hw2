157fbe6464d640fc14801c333f5cf7c609843a7e
Jasper <jkolp@csumb.edu>
2019-11-06 12:22:21 -0800

    asdf

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,26 @@ const itemOne = 999.99;
 const itemTwo = 1399.99;
 var itemOnecount = localStorage.getItem("itemOne-count");
 let itemTwocount = localStorage.getItem("itemTwo-count");
+var totalPrice = ((itemOnecount * itemOne)+(itemTwocount * itemTwo)).toFixed(2);
 
-console.log(itemOnecount);
-console.log(itemTwocount);
+console.log("Item 1 Count : " + itemOnecount);
+console.log("Item 2 Count : " + itemTwocount);
+console.log("Total price is : " + totalPrice);
+
+displayPrices();
+function displayPrices()
+{
+	//Item One
+	$("#itemOne-qty").html(itemOnecount);
+ 	$("#itemOne-price").html((itemOne * itemOnecount).toFixed(2));
+
+ 	//Item Two
+ 	$("#itemTwo-qty").html(itemTwocount);
+	$("#itemTwo-price").html((itemTwo * itemTwocount).toFixed(2));
+
+	// Total Price
+	$(".total-price").html(totalPrice);
+}
 
  $(".itemOne-minus").on("click", function(){
  	if(itemOnecount > 1){
@@ -12,7 +29,7 @@ console.log(itemTwocount);
  		$("#itemOne-price").html(itemOne * itemOnecount.toFixed(2));
  		localStorage.setItem("itemOne-count", itemOnecount);
  	}
- 	$(".total-price").html(((itemOnecount * itemOne)+(itemTwocount * itemTwo)).toFixed(2));
+ 	$(".total-price").html(totalPrice);
 });
 
 
